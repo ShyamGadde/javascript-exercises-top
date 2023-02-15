fbfaf7f68dc847a29aff84d04ea624864276af39
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2023-02-15 18:12:17 +0530

    Solve exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,7 @@
-const repeatString = function() {
-
+const repeatString = function(str, count) {
+    let output = "";
+    for (let i = 0; i < count; i++) output += str;
+    return (count < 0) ? "ERROR" : output;
 };
 
 // Do not edit below this line
